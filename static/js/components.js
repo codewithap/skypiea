d538a92a7d4884f9d240eb547130adb84bc015b9
Arijit Paine <99323868+codewithap@users.noreply.github.com>
2024-04-16 13:17:06 +0530

    Add files via upload

--- static/js/components.js
+++ static/js/components.js
@@ -113,3 +113,53 @@ function scrollX(slider){
     slider.scrollLeft = scrollLeft - walk;
   });
 }
+
+/////  slider /////
+let slider = document.querySelector('.slideShow_mobile .slider');
+console.log(slider.offsetWidth)
+slider.style.height = `${1.3*(slider.offsetWidth)}px`;
+let counter = 0;
+  // image Slide show //
+
+fetch('https://aniapi-eight.vercel.app/api/topAnimes?page=1')
+  .then(response => {
+      return response.json();
+  }).then(data => {
+    let SlidesList = data['items'];
+    for (let i = 0; i < SlidesList.length; i++) {
+  slider.innerHTML += `<div class='a' style='left: ${i*100}%'>
+  <div class='btn'><span class='title'><p>${SlidesList[i]['title']}</p>
+  
+  <button class='watchBtn'><span class="material-symbols-rounded">play_arrow</span> START WATCHING</button></span><span class='bg'></span><img src='${SlidesList[i]['imgs']['webp']['large']}'>
+  </div></div>`;
+}
+  }).catch(error => {
+    console.error(error)
+  });
+
+function slide() {
+  let slides = slider.querySelectorAll('.a');
+  if (Math.abs(counter) > 9) {
+    counter = 0;
+  }
+  for (let i = 0; i < 10; i++) {
+    slides[i].style.transform = `translateX(-${(Math.abs(counter))*100}%)`;
+  } }
+  
+let prev = () => {
+  if (counter > 0) {
+    counter -= 1;
+    slide()
+  }
+  
+  else if (counter == 0) {
+    counter = 9;
+    slide()
+  }
+} 
+
+let next = () => {
+  counter += 1;
+  slide()
+}
+setInterval(next, 8000);
